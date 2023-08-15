93657e88091e0950f431cd1157d097c6431775c4
Mark Tikhonov <mtik.philosopher@gmail.com>
2023-08-15 14:37:44 -0700

    fixes added to log.h file as well as println and printf options

--- Control-Panel/lib/logging/log.h
+++ Control-Panel/lib/logging/log.h
@@ -19,31 +19,46 @@
 #define DEBUG_LOG_LABLE "[DEBUG]: "
 #define VERBOSE_LOG_LABLE "[VERBOSE] "
 
-#define __LOG(message, ...) Serial.println(FLASH_PROCESS(message), ##__VA_ARGS__)
+#define __LOG(str) Serial.print(FLASH_PROCESS(str))
+#define __LOG_N(str) Serial.println(FLASH_PROCESS(str))
+#define __LOG_F(str, ...) do{ Serial.printf(str, ##__VA_ARGS__); Serial.print('\n'); } while(0)
 
-#define log_e(message, ...)
-#define log_i(message, ...)
-#define log_d(message, ...)
-#define log_v(message, ...)
+#define log_e_f(message, ...)
+#define log_i_f(message, ...)
+#define log_d_f(message, ...)
+#define log_v_f(message, ...)
+
+#define log_e(message)
+#define log_i(message)
+#define log_d(message)
+#define log_v(message)
 
 #if LOGGING_LEVEL > 0
 #undef log_e
-#define log_e(message, ...) do{Serial.print(FLASH_PROCESS(ERROR_LOG_LABEL)); __LOG(message, ##__VA_ARGS__);} while(0)
+#undef log_e_f
+#define log_e(message) do{ __LOG(ERROR_LOG_LABEL); __LOG_N(message);} while(0);
+#define log_e_f(message, ...) do{ __LOG(ERROR_LOG_LABEL); __LOG(message, ##__VA_ARGS__);} while(0)
 #endif
 
 #if LOGGING_LEVEL > 1
 #undef log_i
-#define log_i(message, ...) do{Serial.print(FLASH_PROCESS(INFO_LOG_LABLE)); __LOG(message, ##__VA_ARGS__);} while(0)
+#undef log_i_f
+#define log_i(message) do{ __LOG(INFO_LOG_LABLE); __LOG_N(message);} while(0);
+#define log_i_f(message, ...) do{__LOG(INFO_LOG_LABLE); __LOG(message, ##__VA_ARGS__);} while(0)
 #endif
 
 #if LOGGING_LEVEL > 2
 #undef log_d
-#define log_d(message, ...) do{Serial.print(FLASH_PROCESS(DEBUG_LOG_LABLE)); __LOG(message, ##__VA_ARGS__);} while(0)
+#undef log_d_f
+#define log_d(message) do{ __LOG(DEBUG_LOG_LABLE); __LOG_N(messssage);} while(0);
+#define log_d_f(message, ...) do{ __LOG(DEBUG_LOG_LABLE); __LOG_F(message, ##__VA_ARGS__);} while(0)
 #endif
 
 #if LOGGING_LEVEL > 3
 #undef log_v
-#define log_v(message, ...) do{Serial.print(FLASH_PROCESS(VERBOSE_LOG_LABLE)); __LOG(message, ##__VA_ARGS__);} while(0)
+#undef log_v_f
+#define log_v(message) do{ __LOG(VERBOSE_LOG_LABLE); __LOG_N(messssage);} while(0);
+#define log_v_f(message, ...) do{ __LOG(VERBOSE_LOG_LABLE); __LOG_F(message, ##__VA_ARGS__);} while(0)
 #endif
  
  
